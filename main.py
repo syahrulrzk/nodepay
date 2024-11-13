dfb7b79a4128807d23db9184803b6789bd45d9bb
0xmiwe <bhaipemul@gmail.com>
2024-11-13 23:13:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def read_lines(filename):
 class Config:
     def __init__(self):
         self.base_url = 'https://nodepay.org'
-        self.ping_url = 'http://52.17.10.116/api/network/ping'
+        self.ping_url = 'http://52.77.10.116/api/network/ping'
         self.retry_interval = 30
-        self.session_url = 'http://18.136.143.169/api/auth/session'
+        self.session_url = 'http://api.nodepay.ai/api/auth/session'
 
 class Logger:
     @staticmethod
